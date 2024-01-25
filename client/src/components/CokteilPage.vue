4dad6edc1979fca63186723747adfa7cd8fec13b
Demon5611 <d555601@ya.ru>
2024-01-25 23:06:10 +0300

    finish

--- client/src/components/CokteilPage.vue
+++ client/src/components/CokteilPage.vue
@@ -1,45 +1,38 @@
 <template>
     <div class="cocktail-page">
         <h1>{{ form.data?.strDrink }}</h1>
-        <br />
-        <div v-if="form.data && Object.keys(form.data).length > 0">
-            <p v-if="form.data.strCategory">Category: {{ form.data.strCategory }}</p>
+        <div v-if="!cocktailDataAvailable">
+            <router-link :to="{ name: 'NotFound' }">Go to NotFound</router-link>
         </div>
-  
-        <div v-if="form.data && Object.keys(form.data).length > 0">
-            <p v-if="form.data.strAlcoholic">{{ form.data.strAlcoholic }}</p>
+        <div v-else>
+            <div v-if="Object.keys(form.data).length > 0">
+                <p v-if="form.data.strCategory">Category: {{ form.data.strCategory }}</p>
+                <p v-if="form.data.strAlcoholic">{{ form.data.strAlcoholic }}</p>
+                <p v-if="form.data.strGlass">{{ form.data.strGlass }}</p>
+                <p v-if="form.data.strInstructions">Instructions: {{ form.data.strInstructions }}</p>
+            </div>
+            <br />
+            <h3>List of Ingredients:</h3>
+            <ul v-if="Object.keys(form.data).length > 0">
+                <li v-for="(measure, index) in ingredientMeasures" :key="index">
+                    <span>Measure {{ index + 1 }}:</span>
+                    <span>{{ measure }}</span>
+                </li>
+            </ul>
         </div>
-
-        <div v-if="form.data && Object.keys(form.data).length > 0">
-            <p v-if="form.data.strGlass">{{ form.data.strGlass }}</p>
-        </div>
-        <br />
-        <div v-if="form.data && Object.keys(form.data).length > 0">
-            <p v-if="form.data.strGlass">Instructions :{{ form.data.strInstructions }}</p>
-        </div>    
-        <br />
-        <h3>List of Ingredients:</h3>
-        <ul v-if="form.data && Object.keys(form.data).length > 0">
-            <li v-for="(measure, index) in ingredientMeasures" :key="index">
-                <span>Measure {{ index + 1 }}:</span>
-                <span>{{ measure }}</span>
-            </li>
-        </ul>
-    
     </div>
     <div class="thumbnail-container">
-        <img v-if="form.data && form.data.strDrinkThumb" :src="form.data.strDrinkThumb" alt="coctail Thumbnail" loading="lazy" />
+        <img v-if="form.data?.strDrinkThumb" :src="form.data.strDrinkThumb" alt="cocktail Thumbnail" loading="lazy" />
     </div>
 </template>
-  
+
 <script setup>
 import { computed, onMounted, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
+import { useRoute } from 'vue-router'; // Change this line
 import { useCocktailStore } from '../stores/store';
 
 const cocktailStore = useCocktailStore();
 const route = useRoute();
-
 const form = ref({
   data: {
     strDrink: '',
@@ -47,13 +40,15 @@ const form = ref({
     strAlcoholic: '',
     strGlass: '',
     strDrinkThumb: '',
+    strInstructions: '',
   },
 });
+
+const cocktailDataAvailable = computed(() => form.value.data !== undefined);
 
 onMounted(reloadCocktails);
 
 watch(() => route.params.cocktailName, (newCocktailName) => {
-  console.log('New cocktail name:', newCocktailName);
   reloadCocktails(newCocktailName);
 });
 
@@ -70,15 +65,14 @@ const ingredientMeasures = computed(() => {
 });
 
 async function reloadCocktails(newCocktailName = 'margarita') {
-  console.log('Reloading cocktails for:', newCocktailName);
   await cocktailStore.fetchCocktails(newCocktailName);
-  console.log('Fetched cocktails:', cocktailStore.cocktails);
-  
-  if (cocktailStore.cocktails.length > 0) {
+
+  if (Array.isArray(cocktailStore.cocktails) && cocktailStore.cocktails.length > 0) { 
     Object.assign(form.value.data, cocktailStore.cocktails[0]);
   } else {
+    
+    route.push({ name: 'NotFound' });
     console.error('No drinks found for the specified search term');
   }
 }
-
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,29 +3,27 @@ import CocktailPage from '../components/CokteilPage.vue';
 import NotFound from '../components/Page404.vue';
 
 const routes = [
+
   {
-      path: '/default',
-      redirect: '/Margarita',
-    },
-    {
-      path: '/',
-      redirect: '/Margarita',
-    },
-    {
-      path: '/:cocktailName',
-      name: 'CocktailPage',
-      component: CocktailPage,
-    },
-    {
-      path: '/:catchAll(.*)',
-      name: 'NotFound',
-      component: NotFound,
-    },
+    path: '/:cocktailName',
+    name: 'CocktailPage',
+    component: CocktailPage,
+  },
+  {
+    path: '/',
+    redirect: '/Margarita',
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
+
 
 export default router;
